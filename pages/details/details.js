7507575211e9dcad7a26908a659486305eb75557
GgoodstudyDdayup <337292602@qq.com>
2019-12-10 14:25:36 +0800

    8484

--- pages/details/details.js
+++ pages/details/details.js
@@ -6,7 +6,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-    nav: ["详情", "群组", "视频",'淘票'],
+    nav: ["详情", "群组", "视频", '淘票'],
     tabindex: null,
     openclass: 'wr-bottom', //展开更多
     openclass2: 'wr-bottom', //展开更多
@@ -27,11 +27,25 @@ Page({
     priceNum: 1,
     type: 0,
     modaltype: '',
-    taoPiaoList:[],
-    shareFriend:false
+    taoPiaoList: [],
+    shareFriend: false
   },
   // 
-  getTaoPiaoList: function () {
+  addressCopy() {
+    const that = this
+    let addressCopy = that.data.goodsDetails.data.site
+    wx.setClipboardData({
+      data: addressCopy,
+      success(res) {
+        wx.getClipboardData({
+          success(res) {
+            console.log(res.data) // data
+          }
+        })
+      }
+    })
+  },
+  getTaoPiaoList: function() {
     wx.request({
       url: this.data.host + '/index/tao/getMusicTao',
       method: 'get',
@@ -39,14 +53,14 @@ Page({
         music_id: this.data.music_id || this.data.scene,
       },
       success: (res) => {
-       
+
         console.log("淘票界面")
         console.log(res)
         this.setData({
           taoPiaoList: res.data.data
         })
         console.log(this.data.taoPiaoList)
-       
+
       }
     })
   },
@@ -65,7 +79,7 @@ Page({
         this.setData({
           tabindex: 0,
           goodsDetails: res.data,
-          fds:2,
+          fds: 2,
           price: res.data.cate,
           priceMoney: res.data.cate[0]
         })
@@ -83,7 +97,7 @@ Page({
       },
       success: res => {
         console.log('获取讨论组列表')
-         console.log(res)
+        console.log(res)
         this.setData({
           talkList: res.data.data ? res.data.data : []
         })
@@ -102,9 +116,9 @@ Page({
       },
       success: res => {
         console.log('获取往期视频列表')
-         console.log(res)
+        console.log(res)
         this.setData({
-          videoList: res.data.data?res.data.data:[]
+          videoList: res.data.data ? res.data.data : []
         })
         // console.log(this.data.videoList)
       }
@@ -113,12 +127,12 @@ Page({
   /**
    * 展开详情
    */
-  onopen: function () {
+  onopen: function() {
     this.setData({
       openclass: "openone"
     })
   },
-  closexiangqing: function () {
+  closexiangqing: function() {
     this.setData({
       openclass: "wr-bottom"
     })
@@ -126,18 +140,18 @@ Page({
   /**
    * 展开详情2
    */
-  onopen2: function () {
+  onopen2: function() {
     this.setData({
       openclass2: "openone"
     })
   },
-  closexiangqing2: function () {
+  closexiangqing2: function() {
     this.setData({
       openclass2: "wr-bottom"
     })
   },
   // 切换tab栏
-  tabchange: function (e) {
+  tabchange: function(e) {
     this.setData({
       tabindex: e.currentTarget.dataset.index
     })
@@ -158,7 +172,7 @@ Page({
   //   })
   // },
   // 选择票档
-  handlePriceSelect: function (val) {
+  handlePriceSelect: function(val) {
     this.setData({
       priceSelect: val.currentTarget.dataset.index,
       priceMoney: this.data.price[val.currentTarget.dataset.index]
@@ -166,9 +180,9 @@ Page({
     // console.log(this.data.priceMoney)
   },
   // 减少票
-  minusPrice: function () {
+  minusPrice: function() {
     var priceNum = this.data.priceNum
-    if(priceNum <= 1) {
+    if (priceNum <= 1) {
       return false
     } else {
       priceNum--
@@ -178,7 +192,7 @@ Page({
     }
   },
   // 增加票
-  addPrice: function () {
+  addPrice: function() {
     var priceNum = this.data.priceNum
     priceNum++
     this.setData({
@@ -201,20 +215,20 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-   
+  onLoad: function(options) {
+
     console.log(options)
     this.setData({
       music_id: options.musicid,
       type: options.type || 0,
-      scene:options.scene
+      scene: options.scene
     })
     this.getDetails()
     this.getTalkGroup()
     this.getVideoList()
     this.getTaoPiaoList()
   },
-  toDetails: function (e) {
+  toDetails: function(e) {
 
     console.log(e)
     wx.navigateTo({
@@ -224,63 +238,63 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
     this.toast = this.selectComponent('#toast')
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
     this.toast.closeShareFriend()
     console.log(111)
   },
   //  保存图片
-  savePicture:function(){
+  savePicture: function() {
     wx.showModal({
       title: '提示',
       content: '是否保存二维码？',
-      success: function (res) {
+      success: function(res) {
         if (res.confirm) {
-      
+
           wx.downloadFile({
-            url: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1572778857692&di=bacce7c96295808cf8871e050fa2f90a&imgtype=0&src=http%3A%2F%2Fimg5.cache.netease.com%2Fgame%2F2014%2F6%2F12%2F2014061216555650c3d.png',     //仅为示例，并非真实的资源
-            success: function (res) {
+            url: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1572778857692&di=bacce7c96295808cf8871e050fa2f90a&imgtype=0&src=http%3A%2F%2Fimg5.cache.netease.com%2Fgame%2F2014%2F6%2F12%2F2014061216555650c3d.png', //仅为示例，并非真实的资源
+            success: function(res) {
               // 只要服务器有响应数据，就会把响应内容写入文件并进入 success 回调，业务需要自行判断是否下载到了想要的内容
               if (res.statusCode === 200) {
                 wx.saveImageToPhotosAlbum({
@@ -301,13 +315,13 @@ Page({
           })
 
         } else {
-  
+
         }
 
       }
     })
-  
-      
+
+
   }
-  
+
 })
